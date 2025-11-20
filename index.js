c96f3c3867428909f5047ed5bd0fa4ec839756a2
EstoniaDevUser <tomasselbysigma@gmail.com>
2025-11-20 21:18:39 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Bot töötab: ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!tere") {
+    message.reply("Tere! 👋");
+  }
+});
+
+client.login(process.env.TOKEN);
